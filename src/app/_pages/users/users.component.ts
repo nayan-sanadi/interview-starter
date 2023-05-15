b8df3dbca51c0537d6b9a61f50639fa9d485eeb7
nayan.sanadi@clariontech.com <nayan.sanadi@clariontech.com>
2023-05-15 15:02:25 +0530

    user listing UI with API Integration

--- src/app/_pages/users/users.component.ts
+++ src/app/_pages/users/users.component.ts
@@ -1,13 +1,117 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Subject, takeUntil } from 'rxjs';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { UsersService } from './users.service';
 
 @Component({
   selector: 'app-users',
-  //standalone: true,
-  // imports: [CommonModule],
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.scss']
+  styleUrls: ['./users.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({ height: '0px', minHeight: '0', visibility: 'hidden' })),
+      state('expanded', style({ height: '*', visibility: 'visible' })),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
 export class UsersComponent {
 
+  disabledSpinner:boolean=false;
+  loadData:boolean=true;
+  userData:any;
+  newData:any;
+
+
+  columnsToDisplay = ['username','age','gender','email','phone','birthDate'];
+  genderArray=[
+    {
+      name:"male",
+      value:"male",
+    },
+    {
+      name:"female",
+      value:"female",
+    }
+  ];
+  dataSource:any
+
+  save:boolean=false;
+  expandedElement: any;
+  userForm:any=FormGroup;
+  private _unsubscribeAll: Subject<any>;
+
+
+  constructor(
+    private _usersService:UsersService,
+    private formGroup: FormBuilder,
+    
+  ) {
+
+    this._unsubscribeAll = new Subject();
+
+   }
+
+  ngOnInit(): void {
+
+    this.userForm = this.formGroup.group({
+      id:['',[Validators.required]],
+      firstName:['',[Validators.required]],
+      maidenName:['',[Validators.required]],
+      lastName:['',[Validators.required]],
+      age:['',[Validators.required]],
+      gender:['',[Validators.required]],
+      email:['',[Validators.required]],
+      phone:['',[Validators.required]],
+      birthDate:['',[Validators.required]],
+    });
+
+    this.getUserList();
+  }
+  getUserList()
+  {
+    this._usersService.getUserList().pipe(takeUntil(this._unsubscribeAll)).subscribe(res=>{
+     
+      this.userData=res.users;
+      this.userData.forEach((element:any) => {
+        element.show=false
+      });
+      this.loadData=false;
+       
+      });
+  }
+
+  
+  toggleRow(value:any)
+  {
+    const foundElement = this.userData.find((elem:any) => elem !== undefined && elem.id === value.id)    
+    console.log("The found element is " + JSON.stringify(foundElement));
+    const index = this.userData.indexOf(foundElement);
+  
+    this.userData.forEach((element:any,mainindex:any) => {
+      
+      if(index!=mainindex)
+      {
+        element.show=false;
+      }
+    });
+    this.userForm.get('id').setValue(foundElement.id);
+    this.userForm.get('firstName').setValue(foundElement.firstName);
+    this.userForm.get('maidenName').setValue(foundElement.maidenName);
+    this.userData[index].show = !this.userData[index].show;
+  }
+
+
+  saveUser()
+  {
+
+  }
+
+  closeExpendedDiv(value:any)
+  {
+    
+  }
+
 }
+
--- src/app/_pages/users/users.service.ts
+++ src/app/_pages/users/users.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
-@Injectable({
-  providedIn: 'root'
-})
+import { User } from '@app/_state/users/users-store';
+import { environment } from 'environments/environment';
+import { Observable } from 'rxjs';
+@Injectable()
 export class UsersService {
 
-  constructor() { }
+  constructor(
+    private _httpClient:HttpClient
+  ) { }
+
+  getUserList():Observable<any>
+  {
+    return this._httpClient.get<User>(environment.api+'/user');
+  }
 }
